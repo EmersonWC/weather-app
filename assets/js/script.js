f48fe30d4cb0c570506b1541a949f38c8ed86fd7
EmersonWC <emersonarevalo98@gmail.com>
2021-09-17 14:26:25 -0700

    <format date function works in console>

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,14 @@ var cities = [];
 var key = "495437428674bb2867626ec4c0bdcf64";
 
 // format for day 
+function FormatDay(date){
 var date = new Date();
 console.log(date);
+var month = date.getMonth()+1;
+var day = date.getDate();
+
+var dayOutput = date.getFullYear() + '/' +
+(month<10 ? '0' : '') + month + '/' +
+(day<10 ? '0' : '') + day;
+return dayOutput;
+}
