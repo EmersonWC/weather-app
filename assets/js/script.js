667dee9bcff77f4c265a41e9d5c310362d38e2f0
EmersonWC <emersonarevalo98@gmail.com>
2021-09-17 15:31:29 -0700

    <store city makes array in console>

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,51 @@ var dayOutput = date.getFullYear() + '/' +
 (day<10 ? '0' : '') + day;
 return dayOutput;
 }
+
+// Calling function init();
+init();
+
+
+function init (){
+    // retrieve saved cities from localstorage 
+    // then we parse the json string to an object
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    // if cities were recieved from LS, update the cities array to match
+    if (storedCities !== null) {
+        cities = storedCities;
+    }
+    // render citiess to the dom
+    renderCities();
+    console.log(cities);
+}
+
+function storeCities(){
+    //stringify and place "cities" key in LS to the cities array
+    localStorage.setItem("cities", JSON.stringify(cities));
+    console.log(localStorage);
+}
+
+function renderCities() {
+    //clear the citylist element
+    cityList.empty();
+
+for (var i = 0; i < cities.length; i++) {
+    var city = cities[1];
+
+    var li = $("<li>").text(city);
+    li.attr("id", "listC");
+    li.attr("data-city", city);
+    li.attr("class", "list-group-item");
+    console.log(li);
+    cityList.prepend(li);
+}
+// retrieve response weather for first city
+if (!city){
+    return
+}
+else {
+    getResponseWeather(city)
+};
+}
+
